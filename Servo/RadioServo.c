d7fdf09b132f9b0a9e3a1a2eb01ac5fadebfd14d
Nafiz <nafiz6@yahoo.com>
2019-09-10 00:04:07 +0600

    Revert "Revert "Merge branch 'master' of https://github.com/abrar-fahim/Robo-Arm-Gyro-Mimic-ATMega""
    This reverts commit 437908fd8a8eb63c018e0ec8dec8be6f1121f1d3.

--- Servo/RadioServo.c
+++ Servo/RadioServo.c
@@ -31,21 +31,21 @@ volatile int servoTarget3 = 90;
 int getPulseWidth(int angle) {
 	//for servo, full clockwise is 0 degrees, full anticlockwise is 180 degrees
 	//map values from 0 - 180 to 400-2200
-	
-	
+
+
 	int result = angle * 10;
 	result = result + 400;
 	//    double result = angle / 180.0; //= 0-1
 	//    result = result * 1800.0; //=0-1800
 	//    result = result + 400.0; //400-2200
-	
+
 	//    double x = round(result);
 	//    int y = (int) x;
-	
+
 	//extreme values check
 	if(result < 400) return 400;
 	if(result > 2200) return 2200;
-	
+
 	//return y;
 	return result;
 }
@@ -54,45 +54,47 @@ int main(void) {
 	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU));
 	sei();
 	//all servos in port A
-	
-	DDRB = 0x00;	
+
+	DDRB = 0x00;
 	DDRA = 0xFF;
 	TCCR1A |= 1 << WGM11;
 	TCCR1B |= 1 << WGM12 | 1 << WGM13 | 1 << CS10;
 	TIMSK  |= 1 << OCIE1A;
 	ICR1 = 19999;
-	unsigned char startyaw = 0b00110011;
-	unsigned char rollpitch = 0b11111111;
-	unsigned char clawend = 0b11001100;
 
 	sei();
 
 	servo1 = 90;
 	servo2 = 90;
 	servo3 = 90;
-	//state = 0; //0 for counting up, 1 for counting down
 	while(1) {
-		
+
 		//Here, values to servo1 and servo2 etc... will be processed from gyro readings
-		
+
 		if(TCNT1 >= 300 && TCNT1 <= 2300) {
 			if(TCNT1 >= getPulseWidth(servo1) && bit_is_set(PORTA, PINA0)) PORTA &= ~(1 << PINA0);
 			if(TCNT1 >= getPulseWidth(servo2) && bit_is_set(PORTA, PINA1)) PORTA &= ~(1 << PINA1);
 			if(TCNT1 >= getPulseWidth(servo3) && bit_is_set(PORTA, PINA2)) PORTA &= ~(1 << PINA2);
-			
+
 		}
-		
+
 		if(TCNT1 < 300 || TCNT1 > 2300) {
-			
+
+           unsigned char c;
+           c = uart_getc();
+           uart_putc(c);
+
+
 		}
-	
-		
-	
+
+
+
 	}
 }
 
 
 ISR(TIMER1_COMPA_vect) {
 	//interrupt called when TCNT1 value reaches ICR1 value (every 20ms)
-	
+	PORTA = 0xFF;
+	servo1 = 90;
 }
