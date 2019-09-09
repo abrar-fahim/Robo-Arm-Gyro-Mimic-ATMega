80d080c21cd0f1ddb1496a28c3472fdafa327133
Nafiz <nafiz6@yahoo.com>
2019-09-10 00:03:11 +0600

    Revert "Update RadioServo.c"
    This reverts commit 02cc5be93440582c0559c069aef8dc47e00a3ca7.

--- Servo/RadioServo.c
+++ Servo/RadioServo.c
@@ -58,7 +58,7 @@ int main(void) {
 	DDRB = 0x00;
 	DDRA = 0xFF;
 	TCCR1A |= 1 << WGM11;
-	TCCR1B |= 1 << WGM12 | 1 << WGM13 | 1 << CS11;
+	TCCR1B |= 1 << WGM12 | 1 << WGM13 | 1 << CS10;
 	TIMSK  |= 1 << OCIE1A;
 	ICR1 = 19999;
 
